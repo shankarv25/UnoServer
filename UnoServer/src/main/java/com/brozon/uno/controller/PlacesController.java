db8f31968df5c0603cc4a0b384bdcba0dc12b88c
unknown <venkitac@venkitac-W7.macromedia.com>
2014-02-18 01:02:27 +0530

    Adding basic GET functions for places controller

--- UnoServer/src/main/java/com/brozon/uno/controller/PlacesController.java
+++ UnoServer/src/main/java/com/brozon/uno/controller/PlacesController.java
@@ -1,10 +1,16 @@
 package com.brozon.uno.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.brozon.uno.model.Place;
 
 @Controller
 @RequestMapping(value = "/places")
@@ -12,9 +18,19 @@ public class PlacesController {
 	
 	@RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-	public String index() {
+	public List<Place> index() {
 		System.out.println("Places index");
-		return "{}";
+		List<Place> places = new ArrayList<Place>();
+		places.add(new Place(1, "Gangtok"));
+		places.add(new Place(2, "Shillong"));
+		return places;
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public Place getPlace(@PathVariable long id) {
+		System.out.println("Place " + id);
+		return new Place(1, "Gangtok");
 	}
 
 }
